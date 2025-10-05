e36414765e28820ce4487bce71dcd56b13f5530e
soph <sophia@javamate.net>
2025-10-05 17:24:37 -0400

    Initial commit

---
+++ src/Ass01_RockPaperScissors.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Ass01_RockPaperScissors {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String playOrNot = "Y";
+        String playerA = "";
+        String playerB = "";
+
+        do {
+            do {
+                System.out.println("Player A, please enter your choice, R, P, or S: ");
+                playerA = in.nextLine();
+                in.nextLine();
+            } while(playerA.equalsIgnoreCase("R, P, S"));
+
+            do {
+                System.out.println("Player B, please enter your choice: R, P, or S: ");
+                playerB = in.nextLine();
+                in.nextLine();
+            } while(playerB.equalsIgnoreCase("R, P, S"));
+
+            if(playerA.equalsIgnoreCase("R"))
+            {
+                if(playerB.equalsIgnoreCase("R"))
+                {
+                    System.out.println("You both chose Rock, it's a tie!");
+                }
+                else if(playerB.equalsIgnoreCase("P"))
+                {
+                    System.out.println("Paper covers Rock, Player B wins!");
+                }
+                else
+                    System.out.println("Rock breaks Scissors, Player A wins!");
+            }
+            else if(playerA.equalsIgnoreCase("P"))
+            {
+                System.out.println("processing...");
+            }
+            else
+            {
+                System.out.println("I really want this to work.");
+            }
+
+            } while(playerA.equalsIgnoreCase("R, P, S"));
+
+            in.nextLine();
+            playOrNot = "Y";
+            System.out.println("Press Y to keep playing or any other key to stop playing: ");
+            playOrNot = in.nextLine();
+        } while(playOrNot.equalsIgnoreCase("Y"));
+    }
